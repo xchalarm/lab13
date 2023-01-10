ed8d09e8077ca60ac625c43a4e0c2e5d51e92736
xchalarm <tharana4671@gmail.com>
2023-01-10 15:14:27 +0700

    13_1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+int fibonacci(int x){
+    if(x == 0) return x=0;
+    else if(x == 1) return x=1;
+    else if(x > 1) {
+    return x=fibonacci(x-1) + fibonacci(x-2);
+    }
+    else return 0;
+}   
